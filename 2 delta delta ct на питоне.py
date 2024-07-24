7263e1a2b4981cfeaeb1ad330c8c398f8ef28245
Ke1m <Maxorlee@yandex.ru>
2024-07-24 21:24:15 +0300

    Add files via upload

---
+++ 2 delta delta ct на питоне.py
@@ -0,0 +1,250 @@
+Ct_гена1=float(input("Ct гена 1:"))
+Ct_гена2=float(input("Ct гена 2:"))
+Ct_гена3=float(input("Ct гена 3:"))
+Ct_гена4=float(input("Ct гена 4:"))
+Ct_гена5=float(input("Ct гена 5:"))
+Ct_гена6=float(input("Ct гена 6:"))
+Ct_гена7=float(input("Ct гена 7:"))
+Ct_гена8=float(input("Ct гена 8:"))
+Ct_гена9=float(input("Ct гена 9:"))
+Ct_гена10=float(input("Ct гена 10:"))
+Ct_гена11=float(input("Ct гена 11:"))
+Ct_гена12=float(input("Ct гена 12:"))
+Ct_гена13=float(input("Ct гена 13:"))
+Ct_гена14=float(input("Ct гена 14:"))
+Ct_гена15=float(input("Ct гена 15:"))
+Ct_гена16=float(input("Ct гена 16:"))
+Ct_гена17=float(input("Ct гена 17:"))
+Ct_гена18=float(input("Ct гена 18:"))
+Ct_гена19=float(input("Ct гена 19:"))
+Ct_гена20=float(input("Ct гена 20:"))
+Ct_гена21=float(input("Ct гена 21:"))
+Ct_гена22=float(input("Ct гена 22:"))
+Ct_гена23=float(input("Ct гена 23:"))
+Ct_гена24=float(input("Ct гена 24:"))
+Ct_гена25=float(input("Ct гена 25:"))
+Ct_гена26=float(input("Ct гена 26:"))
+Ct_гена27=float(input("Ct гена 27:"))
+Ct_гена28=float(input("Ct гена 28:"))
+Ct_гена29=float(input("Ct гена 29:"))
+Ct_гена30=float(input("Ct гена 30:"))
+Ct_гена31=float(input("Ct гена 31:"))
+Ct_гена32=float(input("Ct гена 32:"))
+
+Ct_GAPDH1=float(input("Ct GAPDH 1:"))
+Ct_GAPDH2=float(input("Ct GAPDH 2:"))
+Ct_GAPDH3=float(input("Ct GAPDH 3:"))
+Ct_GAPDH4=float(input("Ct GAPDH 4:"))
+Ct_GAPDH5=float(input("Ct GAPDH 5:"))
+Ct_GAPDH6=float(input("Ct GAPDH 6:"))
+Ct_GAPDH7=float(input("Ct GAPDH 7:"))
+Ct_GAPDH8=float(input("Ct GAPDH 8:"))
+Ct_GAPDH9=float(input("Ct GAPDH 9:"))
+Ct_GAPDH10=float(input("Ct GAPDH 10:"))
+Ct_GAPDH11=float(input("Ct GAPDH 11:"))
+Ct_GAPDH12=float(input("Ct GAPDH 12:"))
+Ct_GAPDH13=float(input("Ct GAPDH 13:"))
+Ct_GAPDH14=float(input("Ct GAPDH 14:"))
+Ct_GAPDH15=float(input("Ct GAPDH 15:"))
+Ct_GAPDH16=float(input("Ct GAPDH 16:"))
+Ct_GAPDH17=float(input("Ct GAPDH 17:"))
+Ct_GAPDH18=float(input("Ct GAPDH 18:"))
+Ct_GAPDH19=float(input("Ct GAPDH 19:"))
+Ct_GAPDH20=float(input("Ct GAPDH 20:"))
+Ct_GAPDH21=float(input("Ct GAPDH 21:"))
+Ct_GAPDH22=float(input("Ct GAPDH 22:"))
+Ct_GAPDH23=float(input("Ct GAPDH 23:"))
+Ct_GAPDH24=float(input("Ct GAPDH 24:"))
+Ct_GAPDH25=float(input("Ct GAPDH 25:"))
+Ct_GAPDH26=float(input("Ct GAPDH 26:"))
+Ct_GAPDH27=float(input("Ct GAPDH 27:"))
+Ct_GAPDH28=float(input("Ct GAPDH 28:"))
+Ct_GAPDH29=float(input("Ct GAPDH 29:"))
+Ct_GAPDH30=float(input("Ct GAPDH 30:"))
+Ct_GAPDH31=float(input("Ct GAPDH 31:"))
+Ct_GAPDH32=float(input("Ct GAPDH 32:"))
+
+
+deltaCt1=Ct_гена1-Ct_GAPDH1
+deltaCt2=Ct_гена2-Ct_GAPDH2
+deltaCt3=Ct_гена3-Ct_GAPDH3
+deltaCt4=Ct_гена4-Ct_GAPDH4
+deltaCt5=Ct_гена5-Ct_GAPDH5
+deltaCt6=Ct_гена6-Ct_GAPDH6
+deltaCt7=Ct_гена7-Ct_GAPDH7
+deltaCt8=Ct_гена8-Ct_GAPDH8
+deltaCt9=Ct_гена9-Ct_GAPDH9
+deltaCt10=Ct_гена10-Ct_GAPDH10
+deltaCt11=Ct_гена11-Ct_GAPDH11
+deltaCt12=Ct_гена12-Ct_GAPDH12
+deltaCt13=Ct_гена13-Ct_GAPDH13
+deltaCt14=Ct_гена14-Ct_GAPDH14
+deltaCt15=Ct_гена15-Ct_GAPDH15
+deltaCt16=Ct_гена16-Ct_GAPDH16
+deltaCt17=Ct_гена17-Ct_GAPDH17
+deltaCt18=Ct_гена18-Ct_GAPDH18
+deltaCt19=Ct_гена19-Ct_GAPDH19
+deltaCt20=Ct_гена20-Ct_GAPDH20
+deltaCt21=Ct_гена21-Ct_GAPDH21
+deltaCt22=Ct_гена22-Ct_GAPDH22
+deltaCt23=Ct_гена23-Ct_GAPDH23
+deltaCt24=Ct_гена24-Ct_GAPDH24
+deltaCt25=Ct_гена25-Ct_GAPDH25
+deltaCt26=Ct_гена26-Ct_GAPDH26
+deltaCt27=Ct_гена27-Ct_GAPDH27
+deltaCt28=Ct_гена28-Ct_GAPDH28
+deltaCt29=Ct_гена29-Ct_GAPDH29
+deltaCt30=Ct_гена30-Ct_GAPDH30
+deltaCt31=Ct_гена31-Ct_GAPDH31
+deltaCt32=Ct_гена32-Ct_GAPDH32
+
+Ср_delta_контролей=((deltaCt1+deltaCt2+deltaCt3+deltaCt4+deltaCt5+deltaCt6+deltaCt7+deltaCt8)/8)
+
+delta_delta_Ct1=deltaCt1-Ср_delta_контролей
+delta_delta_Ct2=deltaCt2-Ср_delta_контролей
+delta_delta_Ct3=deltaCt3-Ср_delta_контролей
+delta_delta_Ct4=deltaCt4-Ср_delta_контролей
+delta_delta_Ct5=deltaCt5-Ср_delta_контролей
+delta_delta_Ct6=deltaCt6-Ср_delta_контролей
+delta_delta_Ct7=deltaCt7-Ср_delta_контролей
+delta_delta_Ct8=deltaCt8-Ср_delta_контролей
+delta_delta_Ct9=deltaCt9-Ср_delta_контролей
+delta_delta_Ct10=deltaCt10-Ср_delta_контролей
+delta_delta_Ct11=deltaCt11-Ср_delta_контролей
+delta_delta_Ct12=deltaCt12-Ср_delta_контролей
+delta_delta_Ct13=deltaCt13-Ср_delta_контролей
+delta_delta_Ct14=deltaCt14-Ср_delta_контролей
+delta_delta_Ct15=deltaCt15-Ср_delta_контролей
+delta_delta_Ct16=deltaCt16-Ср_delta_контролей
+delta_delta_Ct17=deltaCt17-Ср_delta_контролей
+delta_delta_Ct18=deltaCt18-Ср_delta_контролей
+delta_delta_Ct19=deltaCt19-Ср_delta_контролей
+delta_delta_Ct20=deltaCt20-Ср_delta_контролей
+delta_delta_Ct21=deltaCt21-Ср_delta_контролей
+delta_delta_Ct22=deltaCt22-Ср_delta_контролей
+delta_delta_Ct23=deltaCt23-Ср_delta_контролей
+delta_delta_Ct24=deltaCt24-Ср_delta_контролей
+delta_delta_Ct25=deltaCt25-Ср_delta_контролей
+delta_delta_Ct26=deltaCt26-Ср_delta_контролей
+delta_delta_Ct27=deltaCt27-Ср_delta_контролей
+delta_delta_Ct28=deltaCt28-Ср_delta_контролей
+delta_delta_Ct29=deltaCt29-Ср_delta_контролей
+delta_delta_Ct30=deltaCt30-Ср_delta_контролей
+delta_delta_Ct31=deltaCt31-Ср_delta_контролей
+delta_delta_Ct32=deltaCt32-Ср_delta_контролей
+
+
+two_delta_Ct1=(2**(-delta_delta_Ct1))
+two_delta_Ct2=(2**(-delta_delta_Ct2))
+two_delta_Ct3=(2**(-delta_delta_Ct3))
+two_delta_Ct4=(2**(-delta_delta_Ct4))
+two_delta_Ct5=(2**(-delta_delta_Ct5))
+two_delta_Ct6=(2**(-delta_delta_Ct6))
+two_delta_Ct7=(2**(-delta_delta_Ct7))
+two_delta_Ct8=(2**(-delta_delta_Ct8))
+two_delta_Ct9=(2**(-delta_delta_Ct9))
+two_delta_Ct10=(2**(-delta_delta_Ct10))
+two_delta_Ct11=(2**(-delta_delta_Ct11))
+two_delta_Ct12=(2**(-delta_delta_Ct12))
+two_delta_Ct13=(2**(-delta_delta_Ct13))
+two_delta_Ct14=(2**(-delta_delta_Ct14))
+two_delta_Ct15=(2**(-delta_delta_Ct15))
+two_delta_Ct16=(2**(-delta_delta_Ct16))
+two_delta_Ct17=(2**(-delta_delta_Ct17))
+two_delta_Ct18=(2**(-delta_delta_Ct18))
+two_delta_Ct19=(2**(-delta_delta_Ct19))
+two_delta_Ct20=(2**(-delta_delta_Ct20))
+two_delta_Ct21=(2**(-delta_delta_Ct21))
+two_delta_Ct22=(2**(-delta_delta_Ct22))
+two_delta_Ct23=(2**(-delta_delta_Ct23))
+two_delta_Ct24=(2**(-delta_delta_Ct24))
+two_delta_Ct25=(2**(-delta_delta_Ct25))
+two_delta_Ct26=(2**(-delta_delta_Ct26))
+two_delta_Ct27=(2**(-delta_delta_Ct27))
+two_delta_Ct28=(2**(-delta_delta_Ct28))
+two_delta_Ct29=(2**(-delta_delta_Ct29))
+two_delta_Ct30=(2**(-delta_delta_Ct30))
+two_delta_Ct31=(2**(-delta_delta_Ct31))
+two_delta_Ct32=(2**(-delta_delta_Ct32))
+
+
+Ср_two_delta_Ct=((two_delta_Ct1+two_delta_Ct2+two_delta_Ct3+two_delta_Ct4+two_delta_Ct5+two_delta_Ct6+two_delta_Ct7+two_delta_Ct8)/8)
+
+
+fold_change1=two_delta_Ct1/Ср_two_delta_Ct
+fold_change2=two_delta_Ct2/Ср_two_delta_Ct
+fold_change3=two_delta_Ct3/Ср_two_delta_Ct
+fold_change4=two_delta_Ct4/Ср_two_delta_Ct
+fold_change5=two_delta_Ct5/Ср_two_delta_Ct
+fold_change6=two_delta_Ct6/Ср_two_delta_Ct
+fold_change7=two_delta_Ct7/Ср_two_delta_Ct
+fold_change8=two_delta_Ct8/Ср_two_delta_Ct
+fold_change9=two_delta_Ct9/Ср_two_delta_Ct
+fold_change10=two_delta_Ct10/Ср_two_delta_Ct
+fold_change11=two_delta_Ct11/Ср_two_delta_Ct
+fold_change12=two_delta_Ct12/Ср_two_delta_Ct
+fold_change13=two_delta_Ct13/Ср_two_delta_Ct
+fold_change14=two_delta_Ct14/Ср_two_delta_Ct
+fold_change15=two_delta_Ct15/Ср_two_delta_Ct
+fold_change16=two_delta_Ct16/Ср_two_delta_Ct
+fold_change17=two_delta_Ct17/Ср_two_delta_Ct
+fold_change18=two_delta_Ct18/Ср_two_delta_Ct
+fold_change19=two_delta_Ct19/Ср_two_delta_Ct
+fold_change20=two_delta_Ct20/Ср_two_delta_Ct
+fold_change21=two_delta_Ct21/Ср_two_delta_Ct
+fold_change22=two_delta_Ct22/Ср_two_delta_Ct
+fold_change23=two_delta_Ct23/Ср_two_delta_Ct
+fold_change24=two_delta_Ct24/Ср_two_delta_Ct
+fold_change25=two_delta_Ct25/Ср_two_delta_Ct
+fold_change26=two_delta_Ct26/Ср_two_delta_Ct
+fold_change27=two_delta_Ct27/Ср_two_delta_Ct
+fold_change28=two_delta_Ct28/Ср_two_delta_Ct
+fold_change29=two_delta_Ct29/Ср_two_delta_Ct
+fold_change30=two_delta_Ct30/Ср_two_delta_Ct
+fold_change31=two_delta_Ct31/Ср_two_delta_Ct
+fold_change32=two_delta_Ct32/Ср_two_delta_Ct
+
+
+print("fold change 1:",fold_change1)
+print("fold change 2:",fold_change2)
+print("fold change 3:",fold_change3)
+print("fold change 4:",fold_change4)
+print("fold change 5:",fold_change5)
+print("fold change 6:",fold_change6)
+print("fold change 7:",fold_change7)
+print("fold change 8:",fold_change8)
+print("fold change 9:",fold_change9)
+print("fold change 10:",fold_change10)
+print("fold change 11:",fold_change11)
+print("fold change 12:",fold_change12)
+print("fold change 13:",fold_change13)
+print("fold change 14:",fold_change14)
+print("fold change 15:",fold_change15)
+print("fold change 16:",fold_change16)
+print("fold change 17:",fold_change17)
+print("fold change 18:",fold_change18)
+print("fold change 19:",fold_change19)
+print("fold change 20:",fold_change20)
+print("fold change 21:",fold_change21)
+print("fold change 22:",fold_change22)
+print("fold change 23:",fold_change23)
+print("fold change 24:",fold_change24)
+print("fold change 25:",fold_change25)
+print("fold change 26:",fold_change26)
+print("fold change 27:",fold_change27)
+print("fold change 28:",fold_change28)
+print("fold change 29:",fold_change29)
+print("fold change 30:",fold_change30)
+print("fold change 31:",fold_change31)
+print("fold change 32:",fold_change32)
+
+
+
+
+
+
+
+
+
+
+
